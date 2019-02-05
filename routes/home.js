8004a5bad6d1c574795be553082c171f9f5750d1
Kala <ericodes4u@gmail.com>
2019-02-04 23:10:31 -0700

    imported data.json file and minor style updates

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync('./public/data/birds.json').toString());
+console.log(json.birds[1].name);
 
 /* Routes */
 router.get('/', function(req, res, next) {
   res.render('home', {title: "Home"});
 });
 router.get('/birds', function(req, res, next) {
-  res.render('birds', {title: "Birds"});
+  res.render('birds', {title: "Birds", json});
 });
 router.get('/services', function(req, res, next) {
   res.render('services', {title: "Services"});
