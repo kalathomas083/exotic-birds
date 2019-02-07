f7ad9eeb62966c5c80ec332d644c5b3e3da9de09
Kala <ericodes4u@gmail.com>
2019-02-06 23:16:32 -0700

    split json file, updated services page

--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var json = JSON.parse(fs.readFileSync('./public/data/birds.json').toString());
-console.log(json.birds[1].name);
+var birdData = JSON.parse(fs.readFileSync('./public/data/birds.json').toString());
+var products = JSON.parse(fs.readFileSync('./public/data/products.json').toString());
+var services = JSON.parse(fs.readFileSync('./public/data/services.json').toString());
+console.log(birdData.name);
 
 /* Routes */
 router.get('/', function(req, res, next) {
   res.render('home', {title: "Home"});
 });
 router.get('/birds', function(req, res, next) {
-  res.render('birds', {title: "Birds", json});
+  res.render('birds', {title: "Birds", birdData});
 });
 router.get('/services', function(req, res, next) {
-  res.render('services', {title: "Services", json});
+  res.render('services', {title: "Services", services});
 });
 router.get('/products', function(req, res, next) {
-  res.render('products', {title: "Products", json});
+  res.render('products', {title: "Products", products});
 });
 router.get('/contact-us', function(req, res, next) {
   res.render('contact', {title: "Contact Us"});
